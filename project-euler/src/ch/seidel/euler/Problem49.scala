91f9b7c301e2c58e87b2fa479df0ef24acf61d8d
luechtdiode <roland.seidel@interpolar.ch>
2013-06-16 01:28:04 +0200

    Solved the Problem 49

---
+++ project-euler/src/ch/seidel/euler/Problem49.scala
@@ -0,0 +1,88 @@
+package ch.seidel.euler
+
+import scala.annotation.tailrec
+
+object Problem49 {
+  
+  /**
+   * Creates a stream of integers between from and to (ascending and inclusive)
+   * @param from first Integer in the Stream
+   * @param to last Integer in the Stream
+   * @return Stream of Integers in the Range (from..to)
+   */
+  def between(from: Int, to: Int):Stream[Int] =
+    if(from >= to) Stream(from) else from #:: between(from + 1, to)
+  
+  /**
+   * @param n number to validate
+   * @return true if n is a prime-number
+   */
+  def isPrime(n: Int) = {
+    if(n < 2) false
+    else if(n == 2) true
+    else if(n > 2 && (n % 2 == 0)) false
+    else (3 to math.sqrt(n.toDouble).toInt by 2) forall(n % _ != 0)
+  }
+
+  /**
+   * Creates a Stream of prime-numbers
+   * @param s Stream of Integers
+   * @return Stream of prime-numbers
+   */
+  def primes(s: Stream[Int]): Stream[Int] = {
+    if(s.isEmpty) Stream.empty
+    else s.head #:: primes(s.tail filter(isPrime(_)))
+  }
+  
+  /**
+   * Translates the number to an List of Digits
+   * @param n number to transform
+   * @return List of digits from the given number
+   */
+  def digitsOf(n: Int): List[Int] = {
+    if(n > 9) {
+      val d = n % 10
+      d +: digitsOf(n / 10)
+    }
+    else List(n)
+  }
+
+  /**
+   * @param one a number to compare with the other number
+   * @param other a number to compare wit the one number
+   * @return true, if both numbers do have the same digits (ordering is ignored)
+   */
+  def equalDigits(one: Int, other: Int) = {
+    digitsOf(one).sorted == digitsOf(other).sorted
+  }
+  
+  /**
+   * Solves the Problem 49
+   */
+  def solve: String = {
+    /**
+     * Searches recursive for three prime numbers with the given distance
+     * @param one the first prime number
+     * @param distance the distance between the searched prime-numbers
+     * @return all concatenated digits of the three prime-numbers or a empty string if not found.
+     */
+    def findThreePrimes(one: Int, distance: Int): String = { 
+      @tailrec
+      def _findThreePrimes(one: Int, level: Int, digits: String): String = { 
+        val next = one + distance
+        if(isPrime(next) && equalDigits(one, next)) {
+          val newdigits = digits + next.toString
+          if(level < 2) 
+            _findThreePrimes(next, level + 1, newdigits)
+          else newdigits
+        }
+        else ""
+      }
+      _findThreePrimes(one, 1, one.toString)
+    }
+    
+    val distance = 3330
+    (primes(between(1000, 9999)) map(findThreePrimes(_, distance)) filter(_.length > 0)).last
+  }    
+  
+}
---
+++ project-euler/srcTest/ch/seidel/euler/Problem49Test.scala
@@ -0,0 +1,54 @@
+package ch.seidel.euler
+
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import ch.seidel.euler.Problem49._
+
+@RunWith(classOf[JUnitRunner])
+class Problem49Test extends FunSuite {
+  
+  /**
+   * The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases by 3330, is unusual in two ways:
+   *    (i) each of the three terms are prime, and, (ii) each of the 4-digit numbers are permutations of one another.
+   * Thereare no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, exhibiting this property, but there 
+   *    is one other 4-digit increasing sequence.
+   * What 12-digit number do you form by concatenating the three terms in this sequence?
+   */
+  test("between") {
+    assert(List(5,6,7) === between(5,7).toList)
+  }
+  
+  test("isPrime") {
+    assert(false === isPrime(1), "1")
+    assert(true === isPrime(2), "2")
+    assert(true === isPrime(3), "3")
+    assert(true === isPrime(5), "5")
+    assert(true === isPrime(7), "7")
+    assert(List(1,2,3,5,7,11,13,17,19) === primes(between(1, 20)).toList)
+  }
+  
+  test("DigitsOf") {
+    assert(List(1,2,3,4) === digitsOf(1234).sorted)
+    assert(List(1,2,3,4) === digitsOf(4321).sorted)
+    assert(List(1,2,3,4) === digitsOf(2413).sorted)
+  }
+
+  test("EqualDigits") {
+    assert(true === equalDigits(1234, 4321))
+  }
+  
+  test("Three4-digitPrimeTerms with 3330 distance") {
+    val solution = solve 
+    assert(12 === solution.length())
+    assert("148748178147" != solution) // we search the other three primes
+    val p1 = solution take 4 toInt
+    val p2 = solution drop 4 take 4 toInt
+    val p3 = solution drop 8 take 4 toInt
+    
+    assert(true === (isPrime(p1) && p1 + 3330 == p2))
+    assert(true === (isPrime(p2) && p2 + 3330 == p3))
+    assert(true === isPrime(p3))
+  }
+}
