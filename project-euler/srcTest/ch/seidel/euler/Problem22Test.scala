de801297db9ba18b6194644786760ab3c23dca99
luechtdiode <roland.seidel@interpolar.ch>
2013-09-30 00:32:24 +0200

    some simplifications

--- project-euler/srcTest/ch/seidel/euler/Problem22Test.scala
+++ project-euler/srcTest/ch/seidel/euler/Problem22Test.scala
@@ -9,10 +9,6 @@ import java.util.Scanner
 
 @RunWith(classOf[JUnitRunner])
 class Problem22Test extends FunSuite {
-  test("test Index") {
-    assert(charOrder('A') === 1)
-    assert(charOrder('Z') === 26)
-  }
   test("COLIN") {
     assert(calculate((0, 1), "COLIN") === (53, 2))
   }
