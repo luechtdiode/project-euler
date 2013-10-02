505651d8ff16df6e24e99c605873fe41f57c39f4
luechtdiode <roland.seidel@interpolar.ch>
2013-10-02 17:56:38 +0200

    solved problem 27

---
+++ project-euler/src/ch/seidel/euler/Problem27.scala
@@ -0,0 +1,62 @@
+package ch.seidel.euler
+
+import scala.annotation.tailrec
+/**
+ * Quadratic primes
+ * Problem 27
+ * 
+ * Euler discovered the remarkable quadratic formula:
+ * 
+ * n² + n + 41
+ * 
+ * It turns out that the formula will produce 40 primes for the 
+ * consecutive values n = 0 to 39. However, 
+ * when n = 40, 402 + 40 + 41 = 40(40 + 1) + 41 is divisible by 41, 
+ * and certainly when n = 41, 41² + 41 + 41 is clearly divisible by 41.
+ * 
+ * The incredible formula  n² − 79n + 1601 was discovered, which 
+ * produces 80 primes for the consecutive values n = 0 to 79. 
+ * The product of the coefficients, −79 and 1601, is −126479.
+ * 
+ * Considering quadratics of the form:
+ * 
+ *     n² + an + b, where |a| < 1000 and |b| < 1000
+ * 
+ *     where |n| is the modulus/absolute value of n
+ *     e.g. |11| = 11 and |−4| = 4
+ * 
+ * Find the product of the coefficients, a and b, for the quadratic 
+ * expression that produces the maximum number of primes for consecutive 
+ * values of n, starting with n = 0.
+ * 
+ */
+object Problem27 {
+
+  def isPrime(n: Long) = {
+    if(n < 2) false
+    else if(n == 2) true
+    else if(n % 2 == 0) false
+    else (3 to math.sqrt(n.toDouble).toInt by 2) forall(n % _ != 0)
+  }
+
+  def quadraticFormula(n: Int, a: Int, b: Int) = n*n + a*n + b
+  
+  def countPrimes(a: Int, b: Int): Int = {
+    @tailrec
+    def count(n: Int): Int = if(isPrime(quadraticFormula(n, a, b))) count(n+1) else n
+    count(0)
+  }
+  
+  def solve(upperbound: Int) = {
+    val coefficients = for {
+      a <- (-1 * upperbound to upperbound)
+      b <- (-1 * upperbound to upperbound)
+    }
+    yield {
+      (a, b, countPrimes(a, b))
+    }
+    
+    val (a, b, _) = coefficients maxBy{case (_, _, quadratic) => quadratic}
+    a * b
+  }
+}
---
+++ project-euler/srcTest/ch/seidel/euler/Problem27Test.scala
@@ -0,0 +1,26 @@
+package ch.seidel.euler 
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import Problem27._
+
+@RunWith(classOf[JUnitRunner])
+class Problem27Test extends FunSuite {
+  test("Eulers quadratic Formula") {
+    assert(14 === quadraticFormula(2, 3, 4))
+    assert(14 === quadraticFormula(2, 3, 4))
+  }
+  test("simple test") {
+    assert(-41 === solve(42))
+  }
+  test("Product of Coefficients a and b, for the quadratic expr. that produces the max number of primes") {
+    /*
+     *  n² + an + b, where |a| < 1000 and |b| < 1000
+     *  Find the product of the coefficients, a and b, for the quadratic expression 
+     *  that produces the maximum number of primes for consecutive values 
+     *  of n, starting with n = 0
+     */
+    println("The solution of problem27 = " + solve(999))
+  }
+}
